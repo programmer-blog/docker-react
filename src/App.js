9f3b06967181f23c83e2495c97821a583720c63d
Programmer Blog <programmerblog.net@gmail.com>
2024-04-07 09:17:16 +0300

    Update styls and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello World!</h2>
+      <header className="App-header">
+        <h1>My Portfolio</h1>
+        <p>Welcome to my portfolio page!</p>
+        <p>This is a basic example of a single-page React application.</p>
+        <p>Feel free to customize and add your own content!</p>
+      </header>
     </div>
   );
 }
