60c349b8fd95e6a3fa8844b1b6043b246ee4a2cd
Programmer Blog <programmerblog.net@gmail.com>
2024-04-07 09:29:18 +0300

    Update test file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/My Portfolio/i);
+  const linkElement = screen.getByText(/portfolio/i);
   expect(linkElement).toBeInTheDocument();
 });
